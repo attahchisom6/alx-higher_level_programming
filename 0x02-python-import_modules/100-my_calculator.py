8cc97ef4f72024cb2889ff93f1714bf74bf7a6f5
attahchisom6 <attahchisom6@gmail.com>
2022-12-01 23:40:18 +0100

    i have debugged it

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,6 +10,18 @@ if __name__ == "__main__":
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
+import sys
+sys.argv
+k = len(sys.argv) - 1
+if k == 0:
+    print("{} arguments".format(k))
+elif k == 1:
+    print("{}: argument:".format(k))
+    print("{}: {}".format(k, sys.argv[k]))
+else:
+    print("{} arguments".format(k))
+    for p in range(k):
+        print("{}: {}".format(p + 1, sys.argv[p + 1]))
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
