18f57a0be7f5a0109a99bc8aa327de8d66aaae67
attahchisom6 <attahchisom6@gmail.com>
2022-12-01 08:21:43 +0100

    inserting a node at a point where its value is optimized in ascending order

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,25 +10,29 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
-
+	listint_t *begin, *end, *new;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 
-	if (node == NULL || node->n >= number)
+	begin = *head;
+	if (begin == NULL || begin->n >= number)
 	{
-		new->next = node;
-		*head = new;
+		new->next = begin;
+		begin = new;
+		
 		return (new);
 	}
-
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+	else
+	{
+		end = *head;
+		while (end->next != NULL && end->next->n < number)
+			end = end->next;
+		new->next = end->next;
+		end->next = new;
+		
+		return (new);
+	}
+	return (NULL);
 }
