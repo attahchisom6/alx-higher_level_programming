a090ec0a68b404f9e1228ee90ecbcf69b634befd
attahchisom6 <attahchisom6@gmail.com>
2022-12-05 23:51:35 +0100

    getting elements in a list that are mutiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,5 +4,9 @@ def divisible_by_2(my_list=[]):
 
     new_list = []
 
-    new_list = [True if k % 2 == 0 else False for k in range(len(my_list))]
+    for k in range(len(my_list)):
+        if k % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
     return (new_list)
