d5c8a0380e3c3c81bf57b98bf30904b6a5217c87
attahchisom6 <attahchisom6@gmail.com>
2022-12-05 22:23:36 +0100

    printing the maximum value in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    
+    len_list = len(my_list)
+    if len_list == 0:
+        return (None)
+    else:
+        max_num = my_list[0]
+        for k in range(len_list):
+            if my_list[k] > max_num:
+                max_num = my_list[k]
+        return (max_num)
+    return (none)
