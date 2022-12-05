72c3394a2f6c8e0fc885950836f9b0d53731981c
attahchisom6 <attahchisom6@gmail.com>
2022-12-05 14:01:04 +0100

    printing the list in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    p = len(my_list) - 1
-    while p > 0:
-        k = 0
-        while k < p:
-            temp = my_list[k]
-            my_list[k] = my_list[k + 1]
-            my_list[k + 1] = temp
-            k = k + 1
-        p = p - 1
-    for k in my_list:
-        print("{:d}".format(k))
-    print()
+    if not my_list:
+        pass
+    else:
+        p = len(my_list) - 1
+        while p > 0:
+            k = 0
+            while k < p:
+                temp = my_list[k]
+                my_list[k] = my_list[k + 1]
+                my_list[k + 1] = temp
+                k = k + 1
+            p = p - 1
+        for k in my_list:
+            print("{:d}".format(k))
