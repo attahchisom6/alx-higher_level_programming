a6695659084c3313ef19169d4f575c054b825e43
attahchisom6 <attahchisom6@gmail.com>
2022-12-06 04:04:57 +0100

    function to check if a list is a palimlndrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,66 +1,114 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * size_of_list - returns size of list
- * @head: head of listint_t
+ * reverse - reverses the second half of the list
  *
- * Return: int
+ * @h_r: head of the second half
+ * Return: no return
  */
-int size_of_list(listint_t **head)
+void reverse(listint_t **h_r)
 {
-	listint_t *ptr = *head;
-	int i = 0;
+	listint_t *prv;
+	listint_t *crr;
+	listint_t *nxt;
 
-	while (ptr)
+	prv = NULL;
+	crr = *h_r;
+
+	while (crr != NULL)
 	{
-		ptr = ptr->next;
-		i++;
+		nxt = crr->next;
+		crr->next = prv;
+		prv = crr;
+		crr = nxt;
 	}
-	return (i);
-}
 
-
-/**
- * get_n_by_idx - get n value using index
- * @head: head of listint_t
- * @idx: index
- *
- * Return: int
- */
-int get_n_by_idx(listint_t **head, int idx)
-{
-	int i = 0;
-	listint_t *ptr = *head;
-
-	while (ptr)
-	{
-		if (i == idx)
-			break;
-		ptr = ptr->next;
-		i++;
-	}
-	return (ptr->n);
+	*h_r = prv;
 }
 
 /**
- * is_palindrome - checks if list is a palindrome
- * @head: head of listint_t
+ * compare - compares each int of the list
  *
- * Return: true (1) or false (0)
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *tmp1;
+	listint_t *tmp2;
+
+	tmp1 = h1;
+	tmp2 = h2;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palndrome
  */
 int is_palindrome(listint_t **head)
 {
-	int start, end;
+	listint_t *slow, *fast, *prev_slow;
+	listint_t *scn_half, *middle;
+	int isp;
 
-	start = 0;
-	end = size_of_list(head) - 1;
+	slow = fast = prev_slow = *head;
+	middle = NULL;
+	isp = 1;
 
-	for (; start <= end; start++, end--)
+	if (*head != NULL && (*head)->next != NULL)
 	{
-		if (get_n_by_idx(head, start) != get_n_by_idx(head, end))
-			return (0);
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn_half = slow;
+		prev_slow->next = NULL;
+		reverse(&scn_half);
+		isp = compare(*head, scn_half);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		}
 	}
-	return (1);
+
+	return (isp);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,13 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -19,8 +21,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-int size_of_list(listint_t **head);
-int get_n_by_idx(listint_t **head, int idx);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
