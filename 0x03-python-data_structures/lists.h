5a0212c02b824c4b155298fcf8b3972e9bb888b0
attahchisom6 <attahchisom6@gmail.com>
2022-12-06 03:58:39 +0100

    function to check if a list is a palimlndrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,14 +11,16 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+int size_of_list(listint_t **head);
+int get_n_by_idx(listint_t **head, int idx);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
