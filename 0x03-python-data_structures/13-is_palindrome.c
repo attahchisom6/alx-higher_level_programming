c220445c849232deb89ae2ceb02a24236a445451
attahchisom6 <attahchisom6@gmail.com>
2022-12-06 03:54:26 +0100

    function to check if a list is a palimlndrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,64 @@
 #include "lists.h"
 
 /**
- * list_size - this function will get the size of a singly linked
- * list
- * @head:a double pointer to the firt node
+ * size_of_list - returns size of list
+ * @head: head of listint_t
  *
- * Return:size of the linked  list
+ * Return: int
  */
-
-int list_size(listint_t **head)
+int size_of_list(listint_t **head)
 {
-	int size = 0;
-	listint_t *curr = *head;
+	listint_t *ptr = *head;
+	int i = 0;
 
-	while (curr != NULL)
+	while (ptr)
 	{
-		curr = curr->next;
-		size++;
+		ptr = ptr->next;
+		i++;
 	}
-	return (size);
+	return (i);
 }
 
+
 /**
- * get_value_at_idx - this function will get the value of a node
- * give a valid index
- * @head:A double pointer to the first node
- * @idx:index of the node whose value we seek
+ * get_n_by_idx - get n value using index
+ * @head: head of listint_t
+ * @idx: index
  *
- * Return:value at the given index
+ * Return: int
  */
-
-int get_value_at_idx(listint_t **head, int idx)
+int get_n_by_idx(listint_t **head, int idx)
 {
-	int k = 0;
-	listint_t *curr = *head;
+	int i = 0;
+	listint_t *ptr = *head;
 
-	while (curr != NULL)
+	while (ptr)
 	{
-		if (k == idx)
+		if (i == idx)
 			break;
-		curr = curr->next;
-		k++;
+		ptr = ptr->next;
+		i++;
 	}
-	return (curr->n);
+	return (ptr->n);
 }
 
 /**
- * is_palindrome - this function will check if a singly linked
- * list is a palindrome
- * @head:a double pointer to the first node
+ * is_palindrome - checks if list is a palindrome
+ * @head: head of listint_t
  *
- * Return:1 if it is palindrome otherwise 0
+ * Return: true (1) or false (0)
  */
-
 int is_palindrome(listint_t **head)
 {
-	int begin, end;
+	int start, end;
 
-	begin = 0;
-	end = list_size(head);
+	start = 0;
+	end = size_of_list(head) - 1;
 
-	end--;
-	while (begin <= end)
+	for (; start <= end; start++, end--)
 	{
-		int g, h;
-
-		g = get_value_at_idx(head, begin);
-		h = get_value_at_idx(head, end);
-		if (g != h)
+		if (get_n_by_idx(head, start) != get_n_by_idx(head, end))
 			return (0);
-		begin++;
-		end--;
 	}
 	return (1);
 }
