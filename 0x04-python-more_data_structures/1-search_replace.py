72d5068e63f1ad203e94a27e0dc2f153effac733
attahchisom6 <attahchisom6@gmail.com>
2022-12-07 20:14:53 +0100

    search to replace a value from a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,8 @@
 
 def search_replace(my_list, search, replace):
 
-    if not my_list:
-        return (None)
-    else:
-        new_list = my_list.copy()
-        for k in range(len(new_list)):
-            if new_list[k] == search:
-                new_list[k] = replace
+    new_list = my_list.copy()
+    for k in range(len(new_list)):
+        if new_list[k] == search:
+            new_list[k] = replace
     return (new_list)
