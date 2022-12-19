bc9f2af2b33affc71c3cc6e8805d1fa2c3eceb92
attahchisom6 <attahchisom6@gmail.com>
2022-12-19 22:31:06 +0100

    Handling Execption: printing integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pthon3
+
+def safe_print_integer(value):
+    try:
+        if value == int(value):
+            print("{:d}".format(value))
+            return (True)
+    except ValueError:
+        return (False)
