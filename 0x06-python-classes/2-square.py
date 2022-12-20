2242aa144fafe7c1bbae57190671bff7430857e4
attahchisom6 <attahchisom6@gmail.com>
2022-12-20 23:54:17 +0100

    handling error messages while printing square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
             integer argument size
         """
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
