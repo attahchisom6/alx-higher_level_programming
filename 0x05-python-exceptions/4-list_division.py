6c22547b75795738bea4fa5624f1b7f3c1741744
attahchisom6 <attahchisom6@gmail.com>
2022-12-20 03:07:01 +0100

    hadling multiple exceptions when printing list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    new_list = []
+
+    for k in range(list_length):
+        try:
+            result = my_list_1[k] / my_list_2[k]
+
+        except ValueError:
+            result = 0
+
+        except TypeError:
+            print("wrong type")
+
+        except ZeroDivisionError:
+            print("division by 0")
+
+        except IndexError:
+            print("out of range")
+
+        finally:
+            new_list.append(result)
+
+    return (new_list)
