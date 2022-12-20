0616f9f337154a8e1e993baf18cb64c697b2940d
attahchisom6 <attahchisom6@gmail.com>
2022-12-20 01:05:50 +0100

    Division: hanling zero wzception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,10 +5,11 @@ def safe_print_division(a, b):
 
     try:
         result = a / b
-        return (result)
 
     except ZeroDivisionError:
         return (None)
 
     finally:
         print("inside result: {}".format(result))
+
+        return (result)
