8265b37958bc74015094993fd3ea7e7024a55304
attahchisom6 <attahchisom6@gmail.com>
2022-12-21 14:48:54 +0100

    printing squares

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,48 +3,43 @@
 
 
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    """define a square a class"""
 
-    def area(self):
-        """ Method that returns the square are of the object
+    def __init__(self, size=0):
+        """initializing an object to store square size
+
+        Args:
+            @self
+            @size
         """
-        return (self.__size ** 2)
+
+        self.size = size
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
-        """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
+    def area(self):
+        return (self.__size ** 2)
+
     def my_print(self):
-        """ Method that prints a # square according
-        to the size value
-        """
-        if not self.__size:
-            print()
+        if self.__size == 0:
+            print("")
         else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end='')
+            p = 0
+            while p < self.__size:
+                k = 0
+                while k < self.__size:
+                    print("#", end="")
+                    k = k + 1
+                p = p + 1
                 print()
