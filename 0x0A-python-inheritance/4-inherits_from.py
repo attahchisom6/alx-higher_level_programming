56a4df68a2cb2fc5f80815520dca281c7a5aa51c
attahchisom6 <attahchisom6@gmail.com>
2023-01-09 22:45:54 +0100

    determining instances of subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""determing if an instance if from a child class"""
+
+
+def inherits_from(obj, a_class):
+    """checks to see if an instance is from a subclass
+    """
+    if type(obj) is not a_class:
+        return (isinstance(obj, a_class))
