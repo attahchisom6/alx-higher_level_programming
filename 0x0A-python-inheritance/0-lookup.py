856963f1eaecdfb84a5076464a45e8052284dd0f
attahchisom6 <attahchisom6@gmail.com>
2023-01-09 19:08:30 +0100

    printing the mwthods and attribute of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+The function look up returns the list if avalaible attribute
+and methods of a class
+"""
+
+
+def lookup(object):
+    """listing atrributes and methods of a class
+    """
+
+    return (dir(object))
