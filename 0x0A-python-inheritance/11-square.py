7a61326a5107dd4e4ab5025ea2e12269c96f4114
attahchisom6 <attahchisom6@gmail.com>
2023-01-10 04:34:10 +0100

    printing elements of square class with str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+defining a grandchild of BaseGeometry and child of Rectangle
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A class inherit from Rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return (super().area())
+
+    def __str__(self):
+        strr = "[Square]"
+        return (strr + " " + "{:d}/{:d}".format(self.__size, self.__size))
