bf6c590002ac31dd73d7dd1d404b2f1b96ae2c9c
attahchisom6 <attahchisom6@gmail.com>
2023-01-10 02:44:54 +0100

    creating rectangular subclass of BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,5 +26,7 @@ class Rectangle(BaseGeometry):
     """derived class from BaseGeometr"""
     def __init__(self, width, height):
         """initializing child instancs"""
-        self.__width = BaseGeometry.integer_validator(self, "width", width)
-        self.__height = BaseGeometry.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
