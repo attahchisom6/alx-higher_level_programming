eb3fcc23b216e06535ed2af29c02e3dc12ae56bc
attahchisom6 <attahchisom6@gmail.com>
2023-01-11 03:02:29 +0100

    replacing attribute of the student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+definining a student class that details his name age last name
+a dictionary representation of the student informatio  in json
+"""
+
+
+class Student:
+    """A studebt class"""
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """public instance to retrieve dictionary representation of student
+    instance
+    """
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(elem) == str for elem in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """replace all the attribute i  the class student"""
+        for key, value in json.items():
+            self.__dict__[key] = value
