bfc586defb29e8002e859127751656b72b917fcd
attahchisom6 <attahchisom6@gmail.com>
2023-03-15 16:59:07 +0100

    initializing a subclass square of the main class rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+/*
+ * this module is to initalize a child clasd
+ */
 
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-}
-
-module.exports = Square;
+} module.exports = Square;
