4c059bbe87e3c831260809cd36fa19fc25d626e5
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 18:41:43 +0100

    free from musql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     db = connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3],
                  port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4], ))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
+            states ASC", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
