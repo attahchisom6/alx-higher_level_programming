d483396bffe79cdb71273bd9294c1ebb0734f30c
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 16:13:29 +0100

    printing states that start with capttal  N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,12 +9,13 @@ from MySQLdb import connect
 if __name__ == "__main__":
     db = connect(host="localhost", user=argv[1], passwd=argv[2],
                  db=argv[3], port=3306)
-    cuzor = db.cursor()
-    cuzor.execute("SELECT * FROM states WHERE name LIKE BINARY
-    'N%' ORDER BY states.id ASC")
-    states = cuzor.fetchall()
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                    ORDER BY states.id ASC")
+    states = cur.fetchall()
     for state in states:
         print(state)
 
-    cuzor.close()
+    cur.close()
     db.close()
