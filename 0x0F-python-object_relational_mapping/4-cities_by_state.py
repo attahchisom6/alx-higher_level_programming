b35e3de0943ba8dfadd02c3fe86fcb163725cf32
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 20:14:27 +0100

    connecting to a new database that accepts 3 argument

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+a script to create a database hbtn_0e_4_usa that accepts 3 arguments
+"""
+from MySQLdb import connect
+from sys import argv
+
+if __name__ == "__main__":
+    db = connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3],
+                 port=3306)
+
+    cur = db.cursor()
+    cur.execute("FROM * SELECT cities")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
