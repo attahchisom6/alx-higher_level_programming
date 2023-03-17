8b54ab128be6e9e6b060f0b894f172db5cf75430
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 15:04:40 +0100

    printing states that start with capttal  N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+display all states starting with capital letter N
+"""
+from sys import argv
+from MySQLdb import connect
+
+if __name__ == "__main__":
+    db = connect(host="localhost", user=argv[1], passwd=argv[2],
+                 db=argv[3], port=3306)
+    cuzor = db.cusor()
+    cuzor.execute("SELECT * FROM states")
+    states = cuzor.fetchall()
+    for state in states:
+        if states[0] == 'N':
+            print(state)
+
+    cuzor.close()
+    db.close()
