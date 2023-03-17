afaa404ae12f494832cf12c731ecb5da90ca2983
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 15:24:16 +0100

    printing states that start with capttal  N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = connect(host="localhost", user=argv[1], passwd=argv[2],
                  db=argv[3], port=3306)
     cuzor = db.cursor()
-    cuzor.execute("SELECT * FROM states WHERE namme LIKE BINARY
+    cuzor.execute("SELECT * FROM states WHERE name LIKE BINARY
                   'N%'")
     states = cuzor.fetchall()
     for state in states:
