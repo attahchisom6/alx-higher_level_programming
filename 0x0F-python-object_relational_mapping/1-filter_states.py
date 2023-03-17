df2df8531f25e90244e01cec397fd77a9f8351c5
attahchisom6 <attahchisom6@gmail.com>
2023-03-17 17:03:49 +0100

    printing states that start with capttal  N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that lists all `states` with a name starting
-with `N` from the database `hbtn_0e_0_usa`.
 
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
 """
-
-import sys
-import MySQLdb
+display all states starting with capital letter N
+"""
+from sys import argv
+from MySQLdb import connect
 
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    db = connect(host="localhost", user=argv[1], passwd=argv[2],
+                 db=argv[3], port=3306)
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                    ORDER BY states.id ASC")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
