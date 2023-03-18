01dc531565ad94df7228ff65110d29ef7b9097fd
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 00:09:54 +0100

    printing the name of the very state with the first id

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine =
-    create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                  .format(argv[1], argv[2], argv[3]))
+    create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                  format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
 
     session = sessionmaker(bind=engine)
