53d4514340efc8d5b34bed155411a7cd959607c2
attahchisom6 <attahchisom6@gmail.com>
2023-03-18 10:22:10 +0100

    taking a statename as argument and printing all names under it

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3],
                  port=3306)
     cur = db.cursor()
-    cur.execute("SELECT c.name FROM cities as c INNER JOIN states as s \
-            ON c.state_id=s.id WHERE s.name=%s", (argv[4],))
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states \
+            ON cities.state_id=states.id WHERE states.name=%s", (argv[4],))
 
     rows = cur.fetchall()
     store = list(row[0] for row in rows)
