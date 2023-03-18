49532c5f818fa00db5ee922d3c6b78fcb7626f69
attahchisom6 <attahchisom6@gmail.com>
2023-03-18 10:27:18 +0100

    taking a statename as argument and printing all names under it

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
 
+"""
+This Script will take a state name as an argument and return
+all city names availaible to the state
+"""
+from MySQLdb import connect
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3],
+                 port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
+    cur.execute("""SELECT cities.name FROM cities INNER JOIN states \
+            ON cities.state_id=states.id WHERE states.name=%s""", (argv[4],))
+
     rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
+    store = list(row[0] for row in rows)
+    print(*store, end=", ")
+
     cur.close()
     db.close()
