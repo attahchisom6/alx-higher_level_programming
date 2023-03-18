bb299a80290ec9a44f712525b7997166b7a38aa6
attahchisom1clear <attahchisom1@gmail.com>
2023-03-18 19:11:21 +0100

    printing the name of the state with the first id

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,8 +9,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-            .format(argv[1], argv[2], argv[3]))
+    engine =
+    create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
 
     session = sessionmaker(bind=engine)
@@ -19,4 +19,5 @@ if __name__ == "__main__":
     state = session.query(State).order_by(State.id).first()
     if state is None:
         print('Nothing')
-    print("{}: {}".format(state.id, state.name))
+    else:
+        print("{}: {}".format(state.id, state.name))
