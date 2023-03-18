3cdb53aa77e0240bd773eec99d8da46a5231b8c8
attahchisom6 <attahchisom6@gmail.com>
2023-03-18 12:20:17 +0100

    linking our database to a class in python

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""This is the State module.
 
-Contains the State class that inherits from Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+This is the State module
+
+This module handles  Base that inherits from declarative_Base module
+"""
+from sqlalchemy import Integer, Column, String
+from sql.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """This class links to the `states` table of our database.
-
-    Attributes:
-        id (int): id of the state.
-        name (str): name of the state.
+    """
+    This class connects/relates to the states table in our
+    hbtn_0e_4_usa database
+    Args:
+        @id: ud of the element in the state table
+        @name: name of eqch state in the table
     """
 
     __tablename__ = 'states'
-
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+    id = column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = column(String(128), nullable=False)
