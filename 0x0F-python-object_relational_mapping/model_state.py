df39277c63c40a294d142c3585e73e0588fb7360
attahchisom6 <attahchisom6@gmail.com>
2023-03-18 12:30:14 +0100

    linking our database to a class in python

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -21,5 +21,5 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = column(String(128), nullable=False)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
