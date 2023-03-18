be31b375e66951a2f9a9ffff72243ba2d4f95386
attahchisom6 <attahchisom6@gmail.com>
2023-03-18 10:29:43 +0100

    taking a statename as argument and printing all names under it

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     rows = cur.fetchall()
     store = list(row[0] for row in rows)
-    print(*store, sep=", ")
+    print(*store, end=", ")
 
     cur.close()
     db.close()
