4401f89c092a6d5b6a103e5b0f693b6c8e414e67
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 08:20:14 +0100

    printing id of a search string state name, free fron sql injections

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,14 +4,14 @@
 use a state name to search the database, free from sql injectins
 """
 from sys import argv
-from model_base import State, Base
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3036/{}".
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(*argv[1:4]))
-    Base.matadata.create_all(engine)
+    Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
     session = session()
 
