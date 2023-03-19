109bd9d11e6e953a003c4a1660a1efaab6c69834
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 10:43:20 +0100

    deleting all state element in the state table with containing "a"

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+delete all state containing the letter 'a'
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(*argv[1:4]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    delete_states = session.query(State).filter(State.name.contains('a'))
+    for state in delete_states:
+        session.delete(state)
+
+    session.close()
