06e77b8c8e131f7c9a07506ece9fd8ad285b901f
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 09:34:57 +0100

    changing name of state where id=2

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@
 
  if __name__ == "__main__":
      engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                            format(*argv[4]))
+                            format(*argv[1:4]))
      Base.metadata.create_all(engine))
      session = sessionmaker(bind=engine)
      session = session()
