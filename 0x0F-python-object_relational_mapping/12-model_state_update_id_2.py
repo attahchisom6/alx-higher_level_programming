276d544819e8157c3627d3d409d2516cf8815417
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 10:01:25 +0100

    changing name of state where id=2

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,20 +3,20 @@
 """
  change the name of a state where id is 2 in a database
  """
- from sys import argv
- from model_state import State, Base
- from sqlalchemy import create_engine
- from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
- if __name__ == "__main__":
-     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                            format(*argv[1:4]))
-     Base.metadata.create_all(engine))
-     session = sessionmaker(bind=engine)
-     session = session()
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(*argv[1:4]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
 
-     inserted_state = session.query(State).filter_by(id=2).first()
-     inserted_state.name = 'New Mexico'
-     session.commit()
+    inserted_state = session.query(State).filter_by(id=2).first()
+    inserted_state.name = "New Mexico"
+    session.commit()
 
-     session.close()
+    session.close()
