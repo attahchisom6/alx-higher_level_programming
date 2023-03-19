72e086f29f381e99ba2d3522691168611e297aba
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 08:53:12 +0100

    inserting into a database via sqlalchemy

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,7 @@ from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:()@localhost:3306/{}".
                            format(*argv[1:4]))
     Base.metadata.create_all(engine)
