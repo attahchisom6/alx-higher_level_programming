7ab9e255a5f288c437c31a87d8cfc2956d4c153a
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 08:52:05 +0100

    inserting into a database via sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+inserts or add into the database a state
+"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ = "__main__":
+    engine = create_engine("mysql+mysqldb://{}:()@localhost:3306/{}".
+                           format(*argv[1:4]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    newState = State(name="Louisiana")
+    session.add(newState)
+    session.commit()
+
+    print(newState.id)
