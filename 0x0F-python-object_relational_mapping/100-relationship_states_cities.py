55c8ca86ae3acdc042679d13b2065c61e486ff1f
attahchisom1clear <attahchisom1@gmail.com>
2023-03-19 06:09:29 +0100

    module relationship using sqlalchemy

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,7 +10,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".
             format(*argv[1:4]))
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
