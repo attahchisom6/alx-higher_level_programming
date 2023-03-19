461d46399db9f831d4df4a835443a11e7215f027
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 01:12:41 +0100

    printing all states that contains the letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(argv[1:4]))
+                           format(*argv[1:4]))
 
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
