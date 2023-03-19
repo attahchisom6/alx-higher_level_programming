6be98d051f9168b7f9ce48ee22b6ce662b037ffb
attahchisom6 <attahchisom6@gmail.com>
2023-03-19 01:05:37 +0100

    printing all states that contains the letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+prints all states that contain the lettter 'a'
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sql.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    states = session.query(State).filter(State.name.like('%a%'))
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
