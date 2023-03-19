97ab091e206113855d2c81a046a2790bf9fb005e
attahchisom1clear <attahchisom1@gmail.com>
2023-03-19 05:42:16 +0100

    relationships

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This will add a state and a city corresponding to it in the database
+hbtn_0e_100_usa
+"""
+from sys import argv
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+            format(*argv[1:4]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
+
+    session.add(newCity)
+    session.add(newState)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This is the State module
+
+This module handles  Base that inherits from declarative_Base module
+"""
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class connects/relates to the states table in our
+    hbtn_0e_4_usa database
+    Args:
+        @id: ud of the element in the state table
+        @name: name of eqch state in the table
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states", cascade="all, delete")
