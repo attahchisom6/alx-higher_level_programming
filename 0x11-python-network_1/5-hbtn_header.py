bf2e687b06b60988e348fa24cce84a18a64eeb94
attahchisom6 <attahchisom6@gmail.com>
2023-04-01 00:51:22 +0100

    getting value of all headers with a given key

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+from requests import get
+from sys import argv
+
+
+def get_url_value(argv):
+    """
+    gets the value of the header in the url with key 'X-header-Id'
+    """
+    url = get(argv[1])
+    print(url.headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    get_url_value(argv)
