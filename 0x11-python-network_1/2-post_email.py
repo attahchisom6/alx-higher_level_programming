18c7f3910c7d1774523447df192eca409b75bfb3
attahchisom6 <attahchisom6@gmail.com>
2023-03-31 22:19:49 +0100

    sending a post request with the passes url as parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,8 @@ Python script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays the
 body of the response (decoded in utf-8)
 """
-from urllib.request import urlopen, Request, parse
+from urllib import parse
+from urllib.request import urlopen, Request
 from sys import argv
 
 
@@ -13,8 +14,9 @@ def display_url_body(argv):
     """
     value = {"email": argv[2]}
     data = parse.urlencode(value)
+    data = data.encode('utf8')
     url = argv[1]
-    url_data = Request.encode(url, data)
+    url_data = Request(url, data)
 
     with urlopen(url_data) as response:
         response_body = response.read()
