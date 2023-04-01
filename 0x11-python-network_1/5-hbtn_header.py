f822da0c0a6b53d3c4df6c270b5ed580be8a35c0
attahchisom6 <attahchisom6@gmail.com>
2023-04-01 01:45:57 +0100

    getting value of all headers with a given key

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
-"""
-import requests
-import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    from requests import get
+    from sys import argv
+
+    html = get(argv[1])
+    print(html.headers.get('X-Request-Id'))
