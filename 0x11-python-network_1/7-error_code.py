0530152d3dc991286dc28d0b84df391533a0d247
attahchisom6 <attahchisom6@gmail.com>
2023-04-01 07:59:04 +0100

    display responce body  if paramter is given

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from requests import get
+
+    response = get(argv[1])
+    if respnse.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+        print(response)
