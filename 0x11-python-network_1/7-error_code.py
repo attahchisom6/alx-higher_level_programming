1049c94b841645e1d7534744ae0356ac725c96ec
attahchisom6 <attahchisom6@gmail.com>
2023-04-01 08:39:13 +0100

    display responce body  if paramter is given

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
 """
 
 
 if __name__ == "__main__":
-    from requests import get
     from sys import argv
+    from requests import get
 
     response = get(argv[1])
     if response.status_code >= 400:
