9ba8e648925ade26fba68b7ace2ce1738cd1dee9
attahchisom6 <attahchisom6@gmail.com>
2023-04-25 18:38:57 +0100

    fetching response from a url

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2], (response, error) => {
+request.get(process.argv[2], (error, response) => {
   if (error) {
     console.log(error);
   } else {
