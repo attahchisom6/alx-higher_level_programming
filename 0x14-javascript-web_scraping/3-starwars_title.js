b45f5b00ea997641c70ec8774b5082fb8e8b13e2
attahchisom6 <attahchisom6@gmail.com>
2023-04-25 21:10:36 +0100

    fetching starwar titlw from starwars api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -8,7 +8,7 @@ const request = require('request');
 const epiNum = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${epiNum}`;
 
-request.get(url, (error, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
