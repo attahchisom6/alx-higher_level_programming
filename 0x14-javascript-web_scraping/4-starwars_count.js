f4cb45b70bef1ef16d8ce3538f597cdfd041409e
attahchisom6 <attahchisom6@gmail.com>
2023-04-26 11:58:31 +0100

    debugging this

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
-/*
- * fetches a web pags from the star wars api then filter out the number of times the character 'Wedge Antilles' occired
- */
 
-const request = require('require');
-const charId = '18';
+const request = require('request');
 const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    let count = 0;
-    const films = JSON.parse(body).results;
-    films.forEach(movie => {
-      movie.characters.forEach(character => {
-        if (character.includes(charId)) {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
           count += 1;
         }
       });
