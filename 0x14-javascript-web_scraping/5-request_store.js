6c112dc376cf533889a87a12a2c4086b458030a3
attahchisom6 <attahchisom6@gmail.com>
2023-04-26 14:20:32 +0100

    getting wepage from an api then writing it to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,25 @@
 #!/usr/bin/node
-const fs = require('fs');
+
+/*
+ * script to get the content of an Api and store it in a file
+ * Args:
+ *  arg1: web page to fetch
+ *  arg2: name of the file to hold the return web page content
+ */
+
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const url = process.argv[2];
+const file = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const fs = require('fs');
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
