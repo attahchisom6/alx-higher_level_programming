269a6a380437aaa5990ae6b2a833efad2bc1b868
attahchisom6 <attahchisom6@gmail.com>
2023-04-26 19:04:12 +0100

    star war api: getting the names of actors in each episode

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
+/*
+ * A script to get all characters of the star war genre from its api, then print each name per line
+ */
 
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const epiID = process.argv[2];
+const url = `https://swapi.dev/api/films/${epiID}`
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
   }
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
+  const Jdata = JSON.parse(body).characters;
+  Jdata.forEach(character => {
+    request.get(character, (error, response, body) => {
       if (error) {
         console.log(error);
-        return;
+      } else {
+        const Jchar = JSON.parse(body);
+        console.log(Jchar.name);
       }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  }
+    })
+  })
 });
